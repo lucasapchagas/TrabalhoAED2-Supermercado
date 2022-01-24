815fc8b47079d822263fb855f26c58713b1a26a5
Leandro <jose.sousa@icomp.ufam.edu.br>
2022-01-24 17:59:54 -0400

    Adição de comentários no arquivo fila.h
    Signed-off-by: Leandro <jose.sousa@icomp.ufam.edu.br>

--- src/fila.h
+++ src/fila.h
@@ -40,10 +40,36 @@
 */
     void inserirFila(fila* f, cliente c);
 
+/* Função removeFila():
+    Argumentos:
+        -fila* f -> recebe o ponteiro de uma fila
+    Exemplo:
+        - cliente removeFila(fila *f);
+        A função removeFila() recebe o ponteiro de uma fila e 
+        então remove o primeiro cliente da fila e retorna esse cliente.
+        No caso de uma fila vazia, retorna apenas void.
+*/
     cliente removeFila(fila *f);
 
+/* Função mostrarFila():
+    Argumentos:
+        -fila* f -> recebe o ponteiro de uma fila
+    Exemplo:
+        - void mostrarFila(fila* f)
+        A função mostrarFila() recebe o ponteiro da fila que será mostrada(fila* f)
+        e retorna a fila.
+*/
     void mostrarFila(fila* f);
 
-    int haElementos(fila *p);
+
+/* Função haElementos:
+    Argumentos:
+        -fila *f -> recebe o ponteiro de uma fila
+    Exemplo:
+        -int haElementos(fila *f)
+        A função haElementos() verifica se o primeiro elemento de 'fila *f' está vazio
+        caso esteja retorna 0, caso contrário retorna 1.
+*/
+    int haElementos(fila *f);
 
     #endif
