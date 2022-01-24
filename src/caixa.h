d5478abcb8c0dacfa9422f24861e4baefa331898
Leandro <jose.sousa@icomp.ufam.edu.br>
2022-01-24 18:58:20 -0400

    Adição de comentários de descrição no arquivo caixa.h
    Signed-off-by: Leandro <jose.sousa@icomp.ufam.edu.br>

--- src/caixa.h
+++ src/caixa.h
@@ -3,6 +3,7 @@
 
 #include "cliente.h"
 
+/* Tipos exportados */
 typedef struct caixa{
     cliente *vetor;
     char statusVendedor;
@@ -11,10 +12,39 @@ typedef struct caixa{
     int tempoServico;
 } caixa;
 
+/* Funções exportadas*/
+
+/* Função criarCaixa();
+    Argumentos:
+      char:
+        statusVendedor -> recebe o objeto statusVendedor.
+        fatorAgilidade -> recebe o objeto fatorAgilidade.
+      int:
+        identificacao -> recebe o objeto identificacao.
+        tempoServico -> recebe o objeto tempoServico.
+    Exemplo:
+         caixa criarCaixa(char statusVendedor, char fatorAgilidade, int identificacao, int tempoServico)
+        Retorna um elemento caixa com os argumentos como características do elemento.
+*/
 caixa criarCaixa(char statusVendedor, char fatorAgilidade, int identificacao, int tempoServico);
 
+/*Função inserirCaixa():
+    Argumentos:
+        caixa* c -> recebe o objeto ponteiro caixa como argumento.
+        cliente -> recebe uma variável tipo cliente como argumento.
+    Exemplo:
+        inserirCaixa(caixa* c, cliente Cliente)
+        A função recebe uma variável cliente para ser inserida no caixa.
+*/
 void inserirCaixa(caixa* c, cliente Cliente);
 
+/*Função mostrarCaixa():
+    Argumentos:
+        caixa c -> recebe o objeto caixa como argumento.
+    Exemplo:
+        mostrarCaixa(caixa c)
+        A função recebe o objeto caixa e o retorna.
+*/
 void mostrarCaixa(caixa c);
 
 #endif
